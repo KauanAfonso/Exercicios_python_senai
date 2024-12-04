0ac9ea0dacb838bd86d95e78292381f41526536c
kauanAfonso <kauanafon3@gmail.com>
2024-12-04 09:02:52 -0300

    Enviando o exercicio 13

---
+++ exercicios_estudos/exer13.py
@@ -0,0 +1,8 @@
+#Escreva a tabuada de 1 a 10 em um arquivo, linha por linha
+with open ("./exercicios_estudos/arquivo_exer13.txt", 'w') as arquivo:
+    arquivo.write("TABUADA\n")
+    
+    for i in range(1,11):
+        arquivo.writelines(f"---------------------\n")
+        for j in range (1,11):
+            arquivo.writelines(f"Tabuada de {i} x {j} == {i*j}\n")
