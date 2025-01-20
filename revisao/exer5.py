442124e0f2448b3722f96790b2953b7b91718596
Kauan <kauanafon3@gmail.com>
2025-01-20 09:56:03 -0300

    Exer 8

--- revisao/exer5.py
+++ revisao/exer5.py
@@ -1,4 +1,4 @@
 numero = int(input("Digite um número: "))
 
-for i in range(1,numero + 1,1):
+for i in range(1, numero + 1, 1):
     print(i)
--- revisao/exer8.py
+++ revisao/exer8.py
@@ -1,6 +1,6 @@
 def contar_caracteres(palavra):
     dic = {}
-    for i in range( 0, len(palavra) , 1):
+    for i in range(0, len(palavra) , 1):
         dic[i + 1] = palavra[i]
     return dic
 
