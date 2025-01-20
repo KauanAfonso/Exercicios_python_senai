2255765375b7bef8488591e96c4136ff1f736e1b
Kauan <kauanafon3@gmail.com>
2025-01-20 10:11:15 -0300

    Exer 8

--- revisao/exer8.py
+++ revisao/exer8.py
@@ -1,7 +1,21 @@
+# def contar_caracteres(palavra):
+#     dic = {}
+#     for i in range(0, len(palavra) , 1):
+#         dic[i + 1] = palavra[i]
+#     return dic
+
+# print(contar_caracteres("kauan"))
+
+
 def contar_caracteres(palavra):
     dic = {}
-    for i in range(0, len(palavra) , 1):
-        dic[i + 1] = palavra[i]
+    contador = 0
+    for i in palavra:
+        for j in range(len(palavra) - 1 , -1, -1):
+            if i == palavra[j]:
+                contador+=1
+        dic[i] = contador
+        contador = 0
     return dic
 
 print(contar_caracteres("kauan"))
