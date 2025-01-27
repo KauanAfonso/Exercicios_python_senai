59dfac5e469d4cf824cb471d197682a59c0e70e9
Kauan <kauanafon3@gmail.com>
2025-01-27 10:48:40 -0300

    exercicio 10

---
+++ POO/exercicios/exercicio10.py
@@ -0,0 +1,24 @@
+class Livro():
+    def __init__(self, titulo,autor,numero_paginas ):
+        self.titulo = titulo
+        self.autor = autor
+        self.numero_paginas = numero_paginas
+        self.status = "Em estoque"
+        
+    def emprestar(self):
+            self.status = "Emprestado"
+        
+    def devolver(self):
+            self.status = "Em estoque"
+            
+    def verificar_livro(self):
+        if self.status == "Em estoque":
+            print("Livro disponível")
+        else:
+            print("Livro indisponível")
+
+
+Simpson = Livro("Simpsons", "Kauan", 88)
+Simpson.emprestar()
+Simpson.verificar_livro()
+    
