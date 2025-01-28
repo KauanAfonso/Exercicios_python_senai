6cb666286e0d29cdf4506867060b9e0b82b6dd6b
Kauan <kauanafon3@gmail.com>
2025-01-28 11:19:57 -0300

    exercicio 12 parte auxiliada com o dorival

--- POO/exercicios/exercicio12.py
+++ POO/exercicios/exercicio12.py
@@ -4,7 +4,7 @@ class Loja_virtual:
         self.produtos = []
         
     def criar_produtos(self,produto):
-        dic = {"ID":produto.id , "NOME":produto.nome, "PRECOOOOOOO": produto.preco, "DESCRICAO": produto.descricao}
+        dic = {"ID":produto.id , "NOME":produto.nome, "PRECO": produto.preco, "DESCRICAO": produto.descricao}
         self.produtos.append(dic)
         
 
@@ -12,10 +12,7 @@ class Loja_virtual:
         print(self.produtos)
         for produto in self.produtos:
             print(produto)
-            
-    def retornar_produtos(self):
-        return self.produtos
-           
+                      
     
 class Produtos():
     def __init__(self, id, nome, preco, descricao):
@@ -29,15 +26,15 @@ class Produtos():
          
         
 class carrinho_compras(Loja_virtual):
-    def __init__(self):
+    def __init__(self, produtos = []):
         super().__init__()
         self.valor_total = 0
         self.produtos_no_carrinho = []
+        self.produtos = produtos
  
     
     def adicionar_produto(self):
-        produ = self.retornar_produtos()
-        for elemento in produ:
+        for elemento in self.produtos:
             print(elemento)
       
     
@@ -49,13 +46,16 @@ class carrinho_compras(Loja_virtual):
 
 banana = Produtos(1,"banana",200,"Amarelo")
 banana2 = Produtos(2,"banana 2",200,"amarelo")
+
 minha_loja = Loja_virtual()
 
 minha_loja.criar_produtos(banana)
 minha_loja.criar_produtos(banana2)
 
-# minha_loja.mostrar_produtos()
-carrinho = carrinho_compras()
+minha_loja.mostrar_produtos()
+
+
+carrinho = carrinho_compras(minha_loja.produtos)
 carrinho.adicionar_produto()
  
         
