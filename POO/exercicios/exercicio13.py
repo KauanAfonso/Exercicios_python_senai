969945f191e44aa396707bdd73ccc6d7c5d9702e
kauan <kauanafon3@gmail.com>
2025-01-31 10:15:28 -0300

    Estuturando exercicio simples

---
+++ POO/exercicios/exercicio13.py
@@ -0,0 +1,15 @@
+class Agenda():
+    def __init__(self):
+        pass
+    
+    def criar_contato(self, nome, numero):
+        pass
+
+    def excluir_contato(self,numero):
+        pass
+
+    def atualizar_contato(self, numero):
+        pass
+
+
+minha_agenda = Agenda()
