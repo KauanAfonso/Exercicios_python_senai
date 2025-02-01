684900a633e8e10e6579e5436d9a603a46353bba
kauan <kauan>
2025-02-01 11:46:51 -0300

    estruturando exercicio 15

---
+++ POO/exercicios/exercicio15.py
@@ -0,0 +1,15 @@
+class Cartas:
+    def __init__(self):
+        self.cartas = ["A♦️" , "A♠️", "A♥️", "A♣️" , "2♦️" , "2♠️", "2♥️", "2♣️"]
+        
+    def embaralhar_cartas(self):
+        pass
+    
+    def sortear_carta(self):
+        pass
+    
+    def distribuir_cartas(self):
+        pass
+    
+    def jogar_cartas(self):
+        pass
