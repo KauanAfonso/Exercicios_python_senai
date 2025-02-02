749ab57672c2865ddfbffa99901e4057519fcc08
kauan <kauanafon3@gmail.com>
2025-02-02 08:55:43 -0300

    Relembrando criação de txt

--- revisao/exer10.py
+++ revisao/exer10.py
@@ -1,3 +1,6 @@
 with open("Ola_mundo.txt", "w") as arquivo:
     arquivo.writelines("Hello world")
+    for i in range(0,11,1):
+        arquivo.writelines("\n")
+        arquivo.writelines(str(i))
     
