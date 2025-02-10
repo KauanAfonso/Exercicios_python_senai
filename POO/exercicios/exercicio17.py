c90032b8d4bb783f149ca8750d7c5ef6e92aa434
kauan <kauanafon3@gmail.com>
2025-02-10 11:31:16 -0300

    Exercicio 17 iniciando

---
+++ POO/exercicios/exercicio17.py
@@ -0,0 +1,23 @@
+'''
+
+Implemente uma classe chamada “Biblioteca” que represente uma biblioteca virtual. 
+Essa classe deve permitir cadastrar livros, fazer empréstimos, devolver livros e verificar 
+a disponibilidade de um livro.
+
+
+'''
+
+class Biblioteca:
+    def __init__(self):
+        self.livros_estoque = []
+
+    def cadastrar_livros(self):
+        pass
+    def fazer_emprestimos(self):
+        pass
+    
+    def devolver_livros(self):
+        pass
+
+    def verificar_disponibilidade(self):
+        pass
