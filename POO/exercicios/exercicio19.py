e10315caf5f4c1d7ffb914c1bb16116ce0608624
kauan <kauanafon3@gmail.com>
2025-02-11 10:20:08 -0300

    iniciando exercicio 19

---
+++ POO/exercicios/exercicio19.py
@@ -0,0 +1,30 @@
+'''
+
+Implemente uma classe chamada “JogoAdivinhacao” que represente um jogo de 
+adivinhação. Essa classe deve gerar um número aleatório, permitir que o jogador faça 
+palpites e informar se o palpite está correto, informando se é maior ou menor que o 
+número gerado.
+
+
+'''
+import random as r
+
+
+class Jogo_adivinhacao:
+    
+    def __init__(self):
+        self.numero_sorteado = r.randint(1,100)
+    
+    def palpitar(self, numero_usuario):
+        if numero_usuario == self.numero_sorteado:
+            print("Acertou o número!")
+            return True
+        if numero_usuario < self.numero_sorteado:
+            print("O Número é maior")
+            return False
+        if numero_usuario > self.numero_sorteado:
+            print("O Número é menor")
+            return False
+        
+    
+    
