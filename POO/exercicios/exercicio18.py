f76e634ef77b1a1e8bf9ca5a70d717db7b3536fd
kauan <kauanafon3@gmail.com>
2025-02-11 09:01:11 -0300

    iniciando exercicio 18

---
+++ POO/exercicios/exercicio18.py
@@ -0,0 +1,21 @@
+'''
+
+Crie uma classe chamada “Calendario” que represente um calendário anual. Essa 
+classe deve ter métodos para exibir o calendário de um determinado mês, verificar se 
+uma data é feriado e calcular a diferença de dias entre duas datas. 
+
+
+'''
+
+class Calendario:
+    def __init__(self):
+        pass
+
+    def exibir_calendario():
+        pass
+
+    def verificar_feriado():
+        pass
+
+    def calcular_dias():
+        pass
