6c39ab9513163caf2130eef6e6ef4fdd7bd0d82b
kauan <kauanafon3@gmail.com>
2025-02-11 08:16:18 -0300

    funcionalidade de emprestar livro

--- POO/exercicios/exercicio17.py
+++ POO/exercicios/exercicio17.py
@@ -12,15 +12,31 @@ class Biblioteca:
         self.livros_estoque = []
         self.listros_emprestados = []
 
-    def cadastrar_livros(self, titulo, author, qtd):
-        livro = {"titulo": titulo, "author":author , "Quantidade": qtd}
+    def cadastrar_livros(self, codigo,titulo, author, qtd):
+        livro = {"codigo":codigo, "titulo": titulo, "author":author , "Quantidade": qtd}
         return self.livros_estoque.append(livro)
 
-    def fazer_emprestimos(self):
-        pass
+    def fazer_emprestimos(self,nome,email, codigo_livro, quantidade):
+        for livro in self.livros_estoque:
+            if livro["codigo"] == codigo_livro and livro["Quantidade"] > 0 and livro["Quantidade"]>= quantidade:
+                livro["Quantidade"] -= quantidade
+                self.listros_emprestados.append(f"Empréstimo para {nome} no email {email} Código: {livro['codigo']}, título: {livro['titulo']}, autor: {livro['author']} quantidade: {quantidade}")
+                break
+            else:
+                "indiponível"
+        else:
+            "livro não encontrado"
+
     
     def devolver_livros(self):
         pass
 
     def verificar_disponibilidade(self):
         pass
+
+
+
+bibliotea = Biblioteca()
+bibliotea.cadastrar_livros(1234, "O pé grande", "Kauan Afonso", 20)
+bibliotea.fazer_emprestimos("Kauan", "kuan@gmail.com", 1234, 2)
+print(bibliotea.lz)
