a612b097430aaeb11a097a326935b865dbe7bedd
kauan <kauanafon3@gmail.com>
2025-02-11 09:49:41 -0300

    Funcionalidade de exibir calendario com python exercicio 18

--- POO/exercicios/exercicio18.py
+++ POO/exercicios/exercicio18.py
@@ -7,15 +7,36 @@ uma data é feriado e calcular a diferença de dias entre duas datas.
 
 '''
 
+import calendar
+import holidays
+import datetime
+
+lista_feriados = [
+    datetime
+]
+
+
 class Calendario:
     def __init__(self):
+        self.feriados = holidays.Brazil(years=2025)
         pass
 
-    def exibir_calendario():
-        pass
+    def exibir_calendario(mes, ano):
+        print(calendar.month(ano, mes))
 
-    def verificar_feriado():
-        pass
+    def verificar_feriado(self,data):
+
+        data = datetime.date("day", "month", "Year")
+        if data in self.feriados:
+            print("È feriado")
+        else:
+            print("Não é feriado! ")
 
     def calcular_dias():
         pass
+
+
+
+meu_calendario = Calendario
+meu_calendario.exibir_calendario(mes=2, ano=2028)
+meu_calendario.verificar_feriado
